a4c7d7ed74f7d7d01ac4445b8f3a860890ef37f6
Ryan Wolfe <ryan@studiohopfitness.com>
2019-02-04 21:43:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+def dictionary
+  hash = {
+  "hello" => 'hi',
+  "to"    => '2',
+  "two"   => '2',
+  "too"   => '2',
+  "for"   => '4',
+  "four"  => '4',
+  'be'    => 'b',
+  'you'   => 'u',
+  "at"    => "@",
+  "and"   => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split(" ").collect {|word|
+    if dictionary.values_at(word) == " " || nil
+      word
+    else
+      dictionary.values_at(word)
+    end
+  }.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+
+end
+
+def selective_tweet_shortener(tweet)
+
+end
+
+def selective_tweet_truncator(tweet)
+
+end
+
+def shortened_tweet_truncator(tweet)
+
+end
